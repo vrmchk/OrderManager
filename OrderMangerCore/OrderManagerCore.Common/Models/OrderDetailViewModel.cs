a14ec2bb5ac6d865d0657b2fa628e0134a4e5a80
vrmchk <ihorveremchuk8@gmail.com>
2022-10-18 13:54:56 +0300

    OrderDetail validation

--- OrderMangerCore/OrderManagerCore.Common/Models/OrderDetailViewModel.cs
+++ OrderMangerCore/OrderManagerCore.Common/Models/OrderDetailViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace OrderManagerCore.Common.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderManagerCore.Common.Models;
 
 public class OrderDetailViewModel
 {
     public int OrderId { get; set; }
     public int ProductId { get; set; }
+    [Range(0, int.MaxValue)]
     public decimal UnitPrice { get; set; }
+    [Range(1, short.MaxValue)]
     public short Quantity { get; set; }
+    [Range(0, 1)]
     public float Discount { get; set; }
 }
